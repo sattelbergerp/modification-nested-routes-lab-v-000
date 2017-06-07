7ba697e5c19fb9e1bd6f01b0ba7e840969f84623
vbox <sattelbergerp@gmail.com>
2017-06-07 17:23:04 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,10 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if(params[:artist_id] && !Artist.exists?(params[:artist_id]))
+        redirect_to artists_path
+    end
+    @song = Song.new(artist_id: params[:artist_id])
   end
 
   def create
@@ -39,7 +42,17 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+      @artist_id = params[:artist_id]
+    if(params[:artist_id])
+        if @artist = Artist.find_by(id: params[:artist_id])
+            @song = @artist.songs.find_by(id: params[:id])
+            redirect_to artist_songs_path(@artist) unless @song
+        else
+            redirect_to artists_path
+        end
+    else
+        @song = Song.find_by(id: params[:id])
+    end
   end
 
   def update
@@ -64,7 +77,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
